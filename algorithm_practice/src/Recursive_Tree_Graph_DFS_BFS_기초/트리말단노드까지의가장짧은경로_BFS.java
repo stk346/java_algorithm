d39f22a202666cb26c637a08ae1ac3d3c08e3920
stk346 <stk346@gamil.com>
2022-09-11 17:36:55 +0900

    트리말단노드까지의가장짧은경로_BFS 완료.

--- algorithm_practice/src/Recursive_Tree_Graph_DFS_BFS_기초/트리말단노드까지의가장짧은경로_BFS.java
+++ algorithm_practice/src/Recursive_Tree_Graph_DFS_BFS_기초/트리말단노드까지의가장짧은경로_BFS.java
@@ -1,5 +1,4 @@
 /*
-BFS로 풀어야 맞는 문제이지만 DFS 연습삼아 풀어본다.
 아래 그림과 같은 이진트리에서 루트 노드 1에서 말단노르까지의 길이 중 가장 짧은 길이를 구하는 프로그램을 작성하세요.
 각 경로의 길이는 루트노드에서 말단노드까지 가는데 이동하는 횟수, 즉 간선(예지)의 개수를 길이로 하겠습니다.
 트리의 형태는 다음과 같습니다.
@@ -10,19 +9,36 @@ BFS로 풀어야 맞는 문제이지만 DFS 연습삼아 풀어본다.
 */
 package Recursive_Tree_Graph_DFS_BFS_기초;
 
-class Node3 {
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node4 {
     int data;
     Node4 lt, rt;
-    public Node3(int val) {
+    public Node4(int val) {
         data = val;
         lt=rt=null;
     }
 }
-public class 트리말단노드까지의가장짧은경로_DFS {
+public class 트리말단노드까지의가장짧은경로_BFS {
     Node4 root;
-    public int DFS(int L, Node4 root) {
-        if (root.lt==null && root.rt == null) return L;
-        else return Math.min(DFS(L+1, root.lt), DFS(L+1, root.rt));
+    public int BFS(Node4 root) {
+        Queue<Node4> Q = new LinkedList<>();
+        Q.offer(root);
+        int L=0;
+        while(!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i=0; i<len; i++) {
+                Node4 cur = Q.poll();
+                if (cur.lt == null && cur.rt == null) {
+                    return L;
+                }
+                if (cur.lt != null) Q.offer(cur.lt);
+                if (cur.rt != null) Q.offer(cur.rt);
+            }
+            L++;
+        }
+        return L;
     }
 
     public static void main(String[] args) {
@@ -32,7 +48,7 @@ public class 트리말단노드까지의가장짧은경로_DFS {
         tree.root.rt = new Node4(3);
         tree.root.lt.lt = new Node4(4);
         tree.root.lt.rt = new Node4(5);
-        System.out.println(tree.BFS( tree.root));
+        System.out.println(tree.BFS(tree.root));
     }
 }
 
