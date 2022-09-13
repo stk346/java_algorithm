218a1aead4a1400e0e5fbb5f551067a553b7c73b
stk346 <stk346@gamil.com>
2022-09-13 21:32:13 +0900

    동전교환 완료.

--- algorithm_practice/src/DFS_BFS_활용/동전교환.java
+++ algorithm_practice/src/DFS_BFS_활용/동전교환.java
@@ -23,19 +23,35 @@
  */
 package DFS_BFS_활용;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class 동전교환 {
-
-
+    static int n, m, answer = Integer.MAX_VALUE;
+    public void DFS(int L, int sum, Integer[] arr) {
+        if (sum > m) return;
+        if (L >= answer) return;
+        if (sum == m) {
+            answer = Math.min(answer, L);
+        }
+        else {
+            for (int i=0; i<n; i++) {
+                DFS(L+1, sum+arr[i], arr);
+            }
+        }
+    }
     public static void main(String[] args) {
         동전교환 T = new 동전교환();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] inputArray = new int[n];
+        n = sc.nextInt();
+        Integer[] arr = new Integer[n];
         for (int i=0; i<n; i++) {
-            inputArray[i] = sc.nextInt();
+            arr[i] = sc.nextInt();
         }
+        Arrays.sort(arr, Collections.reverseOrder());
+        m = sc.nextInt();
+        T.DFS(0, 0, arr);
+        System.out.println(answer);
     }
 }
